ef0853e516fb51bfcbe61969a7570ffd557f944d
davidinovargas <davidinovargas@gmail.com>
2025-03-18 10:12:52 -0300

    Adicionado método estruturado e encapsulamento

---
+++ JogoDaForca.ConsoleApp/JogoDaForca.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace JogoDaForca.ConsoleApp
+{
+    public static class JogoDaForca
+    {
+        public static char[] letrasEncontradas;
+        public static string palavraEscolhida;
+        public static int quantidadeErros;
+        public static bool jogadorPerdeu;
+        public static bool jogadorGanhou;
+
+        private static string[] palavras = {
+            "TYRANNOSAURUS-REX", "TRICERATOPS", "VELOCIRAPTOR", "BRACHIOSAURUS",
+            "STEGOSAURUS", "ANKYLOSAURUS", "SPINOSAURUS", "ALLOSAURUS", "DIPLODOCUS",
+            "PARASAUROLOPHUS", "IGUANODON", "CARNOTAURUS", "CORYTHOSAURUS", "GIGANOTOSAURUS",
+            "DEINONYCHUS", "EDMONTOSAURUS", "GALLIMIMUS", "SAUROLOPHUS", "PACHYCEPHALOSAURUS",
+            "THERIZINOSAURUS", "STYRACOSAURUS", "ALBERTOSAURUS", "EORAPTOR", "CHASMOSAURUS",
+            "KENTROSAURUS", "HYPSILOPHODON", "MAIASAURA", "MOROS-INTREPIDUS", "RUGOPS", "MICROCERATUS"
+        };
+
+        public static void IniciarNovoJogo()
+        {
+            Random gerador = new Random();
+            palavraEscolhida = palavras[gerador.Next(palavras.Length)];
+            letrasEncontradas = new char[palavraEscolhida.Length];
+
+            Array.Fill(letrasEncontradas, '_');
+            quantidadeErros = 0;
+            jogadorPerdeu = false;
+            jogadorGanhou = false;
+        }
+
+        public static void VerificarLetra(char letra)
+        {
+            bool letraEncontrada = false;
+
+            for (int contador = 0; contador < palavraEscolhida.Length; contador++)
+            {
+                if (letra == palavraEscolhida[contador])
+                {
+                    letrasEncontradas[contador] = letra;
+                    letraEncontrada = true;
+                }
+            }
+
+            if (!letraEncontrada) quantidadeErros++;
+
+            AtualizarEstadoJogo();
+        }
+
+        private static void AtualizarEstadoJogo()
+        {
+            jogadorGanhou = string.Join("", letrasEncontradas) == palavraEscolhida;
+            jogadorPerdeu = quantidadeErros > 5;
+        }
+    }
+}
--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -6,145 +6,89 @@ namespace JogoDaForca.ConsoleApp
     {
         static void Main(string[] args)
         {
-            bool jogarNovamente = true;
-            while(jogarNovamente)
-            { 
-                string[] palavras = {
-                    "TYRANNOSAURUS-REX",
-                    "TRICERATOPS",
-                    "VELOCIRAPTOR",
-                    "BRACHIOSAURUS",
-                    "STEGOSAURUS",
-                    "ANKYLOSAURUS",
-                    "SPINOSAURUS",
-                    "ALLOSAURUS",
-                    "DIPLODOCUS",
-                    "PARASAUROLOPHUS",
-                    "IGUANODON",
-                    "CARNOTAURUS",
-                    "CORYTHOSAURUS",
-                    "GIGANOTOSAURUS",
-                    "DEINONYCHUS",
-                    "EDMONTOSAURUS",
-                    "GALLIMIMUS",
-                    "SAUROLOPHUS",
-                    "PACHYCEPHALOSAURUS",
-                    "THERIZINOSAURUS",
-                    "STYRACOSAURUS",
-                    "ALBERTOSAURUS",
-                    "EORAPTOR",
-                    "CHASMOSAURUS",
-                    "KENTROSAURUS",
-                    "HYPSILOPHODON",
-                    "MAIASAURA",
-                    "MOROS-INTREPIDUS",
-                    "RUGOPS",
-                    "MICROCERATUS"
-                                };
+            while (true)
+            {
+                JogoDaForca.IniciarNovoJogo();
 
-                Random geradorDeNumeros = new Random();
-                int indiceEscolhido = geradorDeNumeros.Next(palavras.Length);
-                string palavraEscolhida = palavras[indiceEscolhido];
-                
-                //[null, null, null, null...]
-                char[]letrasEncontradas = new char[palavraEscolhida.Length];
-                for (int caractereAtual = 0; caractereAtual < letrasEncontradas.Length; caractereAtual++)
+                do
                 {
-                    letrasEncontradas[caractereAtual] = '_';
-                }
-                int quantidadeErros = 0;
-                bool jogadorPerdeu = false;
-                bool jogadorGanhou = false;
+                    Console.Clear();
+                    DesenharForca();
+                    ProcessarEntradaUsuario();
+                    VerificarFimDeJogo();
 
-                 do // faça
-                {
-                
-                string cabecaDesenho = quantidadeErros >= 1 ? "X_x " : " ";
-                string troncoDesenho = quantidadeErros >= 2 ? "|" : " ";
-                string troncoInferiorDesenho = quantidadeErros >= 2 ? " | " : " ";
-                string braco1Desenho = quantidadeErros >= 3 ? "/" : " ";
-                string braco2Desenho = quantidadeErros >= 4 ? "\\" : " ";
-                string pernasDesenho = quantidadeErros >= 5 ? "/ \\" : " ";
- 
-                Console.Clear();
-                Console.WriteLine("----------------------------------------");
-                Console.WriteLine("             Jogo da Forca");
-                Console.WriteLine("----------------------------------------");
-                Console.WriteLine(" ___________        ");
-                Console.WriteLine(" |/        |        ");
-                Console.WriteLine(" |        {0}       ", cabecaDesenho);
-                Console.WriteLine(" |                  ");
-                Console.WriteLine(" |        {0}{1}{2} ", braco1Desenho, troncoDesenho, braco2Desenho);
-                Console.WriteLine(" |        {0}       ", troncoInferiorDesenho);
-                Console.WriteLine(" |        {0}       ", pernasDesenho);
-                Console.WriteLine(" |                  ");
-                Console.WriteLine(" |                  ");
-                Console.WriteLine("_|____    " + String.Join("",letrasEncontradas));
-                Console.WriteLine("ERROS: " + quantidadeErros);
-                Console.WriteLine("-----------------------------------------");
+                } while (!JogoDaForca.jogadorGanhou && !JogoDaForca.jogadorPerdeu);
 
-
-                    //dado um caractere
-
-                    Console.Write("Digite uma letra válida: ");
-                    string entradaInicial = Console.ReadLine()!.ToUpper();
-
-                    if (entradaInicial.Length > 1)
-                    {
-                        Console.WriteLine("Entrada inválida! Digite apenas uma letra. Pressione Enter para tentar novamente.");
-                        Console.ReadLine();
-                        continue;
-                    }
-
-                    bool letraFoiEncontrada = false;
-                    char chute = entradaInicial[0];
-
-                    for (int contadorCaracteres = 0; contadorCaracteres < palavraEscolhida.Length; contadorCaracteres++)
-                {
-                        
-                        char caractereAtual = palavraEscolhida[contadorCaracteres];
-
-                    if (chute == caractereAtual)
-                    {
-                        letrasEncontradas[contadorCaracteres] = caractereAtual;
-                        letraFoiEncontrada = true;
-                    }
-                }
-                    if (letraFoiEncontrada == false)
-                        quantidadeErros++;
-
-                    string palavraEncontradaCompleta = String.Join("",letrasEncontradas);
-                    jogadorGanhou = palavraEncontradaCompleta == palavraEscolhida;
-                    jogadorPerdeu = quantidadeErros > 5;
-
-                    if (jogadorGanhou)
-                {
-                    
-                    Console.WriteLine("----------------------------------------");
-                    Console.WriteLine($"Você acertou a palavra {palavraEscolhida}, parabéns!");
-                    Console.WriteLine("----------------------------------------");
-                }
-
-                else if (jogadorPerdeu)
-                {
-                    Console.WriteLine("----------------------------------------");
-                    Console.WriteLine($"Você perdeu, atingiu os {quantidadeErros} erros, a palavra era: {palavraEscolhida}");
-                    Console.WriteLine("----------------------------------------");
-                }
-
-                } while (jogadorGanhou == false && jogadorPerdeu == false); // enquanto (condição)
-
-                Console.Write("Deseja jogar novamente? (S/N)");
-                string resposta = Console.ReadLine()!.ToUpper();
-                if (resposta != "S")
-                {
-                    jogarNovamente = false;
-                }
-
-                Console.Clear();
+                if (!DesejaContinuar())
+                    break;
             }
+
             Console.WriteLine("Finalizando...");
             Console.ReadLine();
         }
+        static void DesenharForca()
+        {
+
+            string cabecaDesenho = JogoDaForca.quantidadeErros >= 1 ? "X_x " : " ";
+            string troncoDesenho = JogoDaForca.quantidadeErros >= 2 ? "|" : " ";
+            string troncoInferiorDesenho = JogoDaForca.quantidadeErros >= 2 ? " | " : " ";
+            string braco1Desenho = JogoDaForca.quantidadeErros >= 3 ? "/" : " ";
+            string braco2Desenho = JogoDaForca.quantidadeErros >= 4 ? "\\" : " ";
+            string pernasDesenho = JogoDaForca.quantidadeErros >= 5 ? "/ \\" : " ";
+
+            Console.Clear();
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("             Jogo da Forca");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(" ___________        ");
+            Console.WriteLine(" |/        |        ");
+            Console.WriteLine(" |        {0}       ", cabecaDesenho);
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |        {0}{1}{2} ", braco1Desenho, troncoDesenho, braco2Desenho);
+            Console.WriteLine(" |        {0}       ", troncoInferiorDesenho);
+            Console.WriteLine(" |        {0}       ", pernasDesenho);
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine("_|____    " + String.Join("", JogoDaForca.letrasEncontradas));
+            Console.WriteLine("ERROS: " + JogoDaForca.quantidadeErros);
+            Console.WriteLine("-----------------------------------------");
+        }
+
+        //dado um caractere
+        static void ProcessarEntradaUsuario()
+        {
+            Console.Write("Digite uma letra válida: ");
+            string entradaInicial = Console.ReadLine()!.ToUpper();
+
+            if (entradaInicial.Length > 1)
+            {
+                Console.WriteLine("Entrada inválida! Digite apenas uma letra. Pressione Enter para tentar novamente.");
+                Console.ReadLine();
+                Console.ReadLine();
+                return;
+            }
+
+            JogoDaForca.VerificarLetra(entradaInicial[0]);
+        }
+            static void VerificarFimDeJogo()
+            {
+                if (JogoDaForca.jogadorGanhou)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine($"Você acertou a palavra {JogoDaForca.palavraEscolhida}, parabéns!");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else if (JogoDaForca.jogadorPerdeu)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine($"Você perdeu, atingiu {JogoDaForca.quantidadeErros} erros, a palavra era: {JogoDaForca.palavraEscolhida}");
+                    Console.WriteLine("----------------------------------------");
+                }
+            }
+        public static bool DesejaContinuar()
+        {
+            Console.Write("Deseja jogar novamente? (S/N)");
+            return Console.ReadLine()!.ToUpper() == "S";
+        }
+    }  
     }
-}
